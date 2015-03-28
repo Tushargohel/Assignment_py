16eaf5097f1c7f181e0d7ab5857efbea48d01e83
Tushargohel <tushargohel25@gmail.com>
2015-03-28 11:44:53 +0530

    midofied calc

--- calc.py
+++ calc.py
@@ -1,22 +1,30 @@
-x=int(input("enter first number"))
+count=1
+while (count==1):
 
-y=int(input("enter the seconnd number"))
+	
+	x=int(input("enter first number"))
 
-N=int(input(''' for additon press-0
-	for substraction press-1
-	for multiplication press-2
-	for  division press-3
-	for modulo press-4
-	'''
-	))
+	y=int(input("enter the seconnd number"))
 
-if N==0:
-	print("ans is =",x+y)
-elif N==1 :
-	print("ans is =",x-y)
-elif N==2 :
-	print("ans is =",x*y)
-elif N==3 :
-	print("ans  is =",x/y)
-else :
-	print("ans is =",x%y)
+	N=int(input(''' for additon press-0
+		for substraction press-1
+		for multiplication press-2
+		for  division press-3
+		for modulo press-4
+		'''
+		))
+
+	if N==0:
+	    print("ans is =",x+y)
+	elif N==1 :
+	    print("ans is =",x-y)
+	elif N==2 :
+	    print("ans is =",x*y)
+	elif N==3 :
+	    print("ans  is =",x/y)
+	elif N==4:
+	    print("ans is =",x%y)
+	else :
+	    print("Error: you have enter a wrong  number")
+
+    count=input(print("Do you want to continue? (yes-1/no-0)"))
